a8f6660a187d4add4833aa208a609e5bc2927334
abhisheksr8@gmail.com <abhisheksr8@gmail.com>
2025-02-06 11:08:50 +0000

    Initialize Snowflake Automation Jobs project with new dbt project, Airflow jobs, and SQL models.

---
+++ jobs/Duplicate_Snowflake-SQLAirflow-Automation/code/dag.py
@@ -0,0 +1,38 @@
+import os
+import sys
+import pendulum
+from datetime import timedelta
+import airflow
+from airflow import DAG
+from airflow.models.param import Param
+from airflow.decorators import task
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+from test_abhisheks_e2etests_snowflake_automationjobs_project_duplicate_snowflake_sqlairflow_automation.tasks import (
+    Branch_1,
+    ForEachLoop_1_tg,
+    Model_1,
+    Python_0,
+    Python_3,
+    Script_1,
+    TaskGroup_1_tg
+)
+PROPHECY_RELEASE_TAG = "__PROJECT_ID_PLACEHOLDER__/__PROJECT_RELEASE_VERSION_PLACEHOLDER__"
+
+with DAG(
+    dag_id = "test_abhisheks_e2etests_Snowflake_AutomationJobs_Project_Duplicate_Snowflake_SQLAirflow_Automation", 
+    schedule_interval = "0 0 17 2 *", 
+    default_args = {"owner" : "Prophecy", "retries" : 0, "ignore_first_depends_on_past" : True, "do_xcom_push" : True}, 
+    start_date = pendulum.today('UTC'), 
+    catchup = False, 
+    max_active_runs = 1
+) as dag:
+    Branch_1_op = Branch_1()
+    Python_0_op = Python_0()
+    ForEachLoop_1_op = ForEachLoop_1_tg.expand(value = Python_0_op.output)
+    Model_1_op = Model_1()
+    Python_3_op = Python_3()
+    Script_1_op = Script_1()
+    TaskGroup_1_op = TaskGroup_1_tg()
+    Python_0_op >> [ForEachLoop_1_op, TaskGroup_1_op]
+    Model_1_op >> Script_1_op
+    Branch_1_op >> [Python_0_op, Python_3_op]
---
+++ jobs/Duplicate_Snowflake-SQLAirflow-Automation/code/test_abhisheks_e2etests_snowflake_automationjobs_project_duplicate_snowflake_sqlairflow_automation/tasks/Model_1.py
@@ -0,0 +1,42 @@
+from test_abhisheks_e2etests_snowflake_automationjobs_project_duplicate_snowflake_sqlairflow_automation.utils import *
+
+def Model_1():
+    from airflow.operators.python import PythonOperator
+    from datetime import timedelta
+    import os
+    import zipfile
+    import tempfile
+
+    return PythonOperator(
+        task_id = "Model_1",
+        python_callable = invoke_dbt_runner,
+        op_kwargs = {
+          "is_adhoc_run_from_same_project": False,
+          "is_prophecy_managed": False,
+          "run_deps": False,
+          "run_seeds": True,
+          "run_parents": False,
+          "run_children": False,
+          "run_tests": True,
+          "run_mode": "project",
+          "entity_kind": None,
+          "entity_name": None,
+          "project_id": "57657",
+          "git_entity": "commit",
+          "git_entity_value": "testBranchSnowflakeJobs",
+          "git_ssh_url": "https://github.com/abhisheksr8/test_Snowflake_jobs_1.git",
+          "git_sub_path": "",
+          "select": "",
+          "threads": "",
+          "exclude": "",
+          "run_props": " --profile run_profile_snowflake",
+          "envs": {
+            "DBT_DATABRICKS_INVOCATION_ENV": "prophecy", 
+            "DBT_PROFILES_DIR": "/home/airflow/gcs/data", 
+            "DBT_SEND_ANONYMOUS_USAGE_STATS": "false", 
+            "DBT_FULL_REFRESH": "true"
+          }
+        },
+        retries = 1, 
+        retry_delay = timedelta(minutes = 1.0)
+    )
