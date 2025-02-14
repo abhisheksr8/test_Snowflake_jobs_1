c60b720bcfc05d54e4b6292acaa88620e2b81783
abhisheksr8@gmail.com <abhisheksr8@gmail.com>
2025-02-14 00:45:54 +0000

    Initialize Snowflake automation jobs project with new jobs, models, and macros.

---
+++ jobs/Duplicate_Snowflake-SQLAirflow-Automation/code/test_abhisheks_e2etests_snowflake_automationjobs_project_duplicate_snowflake_sqlairflow_automation/utils/functions.py
@@ -0,0 +1,16 @@
+d=10
+
+def squared_numbers(number):
+    print('hello')
+
+    return (number * number) / d
+
+separator=' '
+
+def join_2_strings(d1, d2):
+    return f"{d1}{separator}{d2}"
+
+separator=' '
+
+def delete_me(d1, d2):
+    return f"{d1}{separator}{d2}"
