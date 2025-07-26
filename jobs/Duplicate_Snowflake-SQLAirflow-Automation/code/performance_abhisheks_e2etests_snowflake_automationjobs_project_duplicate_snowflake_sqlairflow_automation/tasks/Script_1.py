ab7aaaacedab8566d552751beee6ea3434e8292e
abhisheksr8@gmail.com <abhisheksr8@gmail.com>
2025-07-26 16:01:46 +0000

    Initialize Snowflake automation jobs with Airflow integration - Add new jobs: Snowflake-SQLAirflow-Automation and Duplicate_Snowflake-SQLAirflow-Automation - Add new SQL entities: test_modelanother_Automation_Snowflake_jobs_model, test_Automation_Snowflake_jobs_model, qa_test_Automation_seed_job_snowflake, and generate_schema_name - Remove README.md content

---
+++ jobs/Duplicate_Snowflake-SQLAirflow-Automation/code/performance_abhisheks_e2etests_snowflake_automationjobs_project_duplicate_snowflake_sqlairflow_automation/tasks/Script_1.py
@@ -0,0 +1,6 @@
+from performance_abhisheks_e2etests_snowflake_automationjobs_project_duplicate_snowflake_sqlairflow_automation.utils import *
+
+def Script_1():
+    from airflow.operators.bash import BashOperator
+
+    return BashOperator(task_id = "Script_1", bash_command = "ls -ltr\n", )
---
+++ jobs/Snowflake-SQLAirflow-Automation/code/performance_abhisheks_e2etests_snowflake_automationjobs_project_snowflake_sqlairflow_automation/utils/__init__.py
@@ -0,0 +1,3 @@
+from .dbt_utils import *
+from .functions import *
+from .prophecy import *
